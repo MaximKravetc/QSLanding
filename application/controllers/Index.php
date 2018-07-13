4053488175840caa53966f35b9d63bcf2993c175
Maxim Kravets <maxim.v.kravetc@gmail.com>
2018-07-14 00:44:26 +0300

    Destionation email updated

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -8,16 +8,13 @@ class IndexController extends ControllerAbstract {
         if ($this->getRequest()->isPost() === true) {
             $email = trim(strip_tags($this->getRequest()->getPost('email', '')));
             $name = trim(strip_tags($this->getRequest()->getPost('name', '')));
-            $ua = $this->getRequest()->getServer('HTTP_USER_AGENT');
-            $ip = $this->getRequest()->getServer('REMOTE_ADDR');
 
             if (!empty($name) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $this->model->addSubscriber($name, $email, $ua, $ip);
 
                 try {
                     $mailObj = new Zend_Mail('utf-8');
-                    $mailObj->addTo('m.v.kravetc@gmail.com');
-                    $mailObj->setFrom('m.v.kravetc@gmail.com');
+                    $mailObj->addTo('chapligin.vitaly@yandex.ru');
+                    $mailObj->setFrom('chapligin.vitaly@yandex.ru');
                     $mailObj->setSubject('Новый пользователь:');
                     $mailObj->setBodyHtml( '<p>' . $name . '</p><p>' . $email . '</p>');
                     $mailObj->send();
--- application/models/Index.php
+++ application/models/Index.php
@@ -1,16 +1,4 @@
 <?php
 
-use Shelby\Dao\Result\Mongodb\Insert;
-
 class IndexModel extends ModelAbstract {
-
-    public function addSubscriber(string $name, string $email, string $ua, string $ip) : Insert {
-        return Dao\Mongodb\Listing\Quantumsystem\Subscribers::getInstance()->insertEntry([
-            'email' => $email,
-            'name'  => $name,
-            'ua'    => $ua,
-            'ip'    => $ip
-        ]);
-    }
-
 }
